c9d3f981ad40161b224043974019bcd781d37a56
Rinigus <rinigus.git@gmail.com>
2020-05-10 20:05:31 +0300

    add update-sdk script

---
+++ scripts/update-sdk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+SFOS=3.3.0.16
+
+sdk-assistant create -y SailfishOS-$SFOS \
+              http://releases.sailfishos.org/sdk/targets/Sailfish_OS-$SFOS-Sailfish_SDK_Tooling-i486.tar.7z
+
+for device in h8216 h8266 h8314 h8324 h8416 h9436
+do
+  sdk-assistant create -y sony-$device-armv7hl \
+     http://releases.sailfishos.org/sdk/targets/Sailfish_OS-$SFOS-Sailfish_SDK_Target-armv7hl.tar.7z
+done
+
+sdk-assistant list
