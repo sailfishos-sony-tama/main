8d75928bf92d6f200ba00841b4652e92cbad01d7
Rinigus <rinigus.git@gmail.com>
2020-12-02 20:16:41 +0200

    add update OBS script

---
+++ scripts/update-from-devel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+diff -q -x community-adaptation-testing -x community-adaptation-devel ../nemo:devel:hw:sony:tama ./ || (echo Difference in dirs, cannot continue && exit 1)
+
+root=`pwd`
+
+echo Syncing
+for d in *; do
+    if [ $d == community-adaptation-testing ]; then
+	continue
+    fi
+    echo Updating $d
+    cd $d
+    rsync -av --delete --exclude .osc/ ../../nemo:devel:hw:sony:tama/$d/ ./
+    cd $root
+done
+
+
