48306ddd0c432821baf72c02013b9b0a1881e4a8
Rinigus <rinigus.git@gmail.com>
2021-04-25 11:09:11 +0300

    fix update-sdk script

--- scripts/update-sdk.sh
+++ scripts/update-sdk.sh
@@ -5,11 +5,15 @@ set -e
 SFOS=4.0.1.48
 
 sdk-assistant create -y SailfishOS-$SFOS http://releases.sailfishos.org/sdk/targets/Sailfish_OS-$SFOS-Sailfish_SDK_Tooling-i486.tar.7z
+curl -O http://releases.sailfishos.org/sdk/targets/Sailfish_OS-$SFOS-Sailfish_SDK_Target-$PORT_ARCH.tar.7z
 for device in h8216 h8266 h8314 h8324 h8416 h9436
 do
-  sdk-assistant create -y sony-$device-armv7hl \
-     http://releases.sailfishos.org/sdk/targets/Sailfish_OS-$SFOS-Sailfish_SDK_Target-aarch64.tar.7z
+  sdk-assistant create -y sony-$device-$PORT_ARCH \
+     Sailfish_OS-$SFOS-Sailfish_SDK_Target-$PORT_ARCH.tar.7z
 done
+
+sdk-assistant create -y SailfishOS-$SFOS-$PORT_ARCH \
+	      Sailfish_OS-$SFOS-Sailfish_SDK_Target-$PORT_ARCH.tar.7z
 
 sdk-assistant list
 echo
